17d7d51b8a434d7aba00ca79b749a255ba4bbeaf
PinixK <bjkim0110@gmail.com>
2022-06-21 22:10:01 +0900

    06/21

---
+++ workspace/Basic/src/JExercise_02.java
@@ -0,0 +1,26 @@
+public class JExercise_02 {
+   public static void main(String[] args) {
+      // 7번
+      // System.out.println("1"+"2");     // 12
+      // System.out.println(true + "");   // true
+      // System.out.println('A' + 'B');   // 131
+      // System.out.println('1' + 2);     // 51
+      // System.out.println('1' + '2');   // 99
+      // System.out.println('J' + "ava"); // Java
+      // System.out.println(true + null); // Error
+
+      // 8번
+      // int x = 1;
+      // int y = 2;
+      // int z = 3;
+
+      // int tmp = x;
+      // x = y;
+      // y = z;
+      // z = tmp;
+
+      // System.out.println("x=" + x);
+      // System.out.println("y=" + y);
+      // System.out.println("z=" + z);
+   }
+}
