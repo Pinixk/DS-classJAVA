334e7134501e092add522340f6317ccbb3d49959
Pinixk <bjkim0110@gmail.com>
2022-06-22 17:07:07 +0900

    06/22

--- workspace/Basic/src/Array/_05_2Dimension.java
+++ workspace/Basic/src/Array/_05_2Dimension.java
@@ -1,5 +1,18 @@
 public class _05_2Dimension {
     public static void main(String[] args) {
-        
+        int[] arr[] = new int[3][3];
+        printArr(arr);
     }
+
+    static void printArr(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) { // arr.length, 행의 길이
+            for (int j = 0; j < arr[i].length; j++) { // arr[i].length, 열의 길이
+                if (j != 0)
+                    System.out.print(',');
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
 }
---
+++ workspace/Basic/src/Array/_06_2Dimention.java
@@ -0,0 +1,6 @@
+public class _06_2Dimention {
+    public static void main(String[] args) {
+        int[][] arr = {{100,100,100},{90,90,90},{80,80,80},{70,70,70}};
+        _05_2Dimension.printArr(arr);
+    }
+}
