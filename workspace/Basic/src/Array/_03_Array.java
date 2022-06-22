6e8bd3f54651bb30c77cfce2412bb9c05bbb80d5
Pinixk <bjkim0110@gmail.com>
2022-06-22 14:08:58 +0900

    06/22

--- workspace/Basic/src/Array/_03_Array.java
+++ workspace/Basic/src/Array/_03_Array.java
@@ -2,13 +2,15 @@ import java.util.Arrays;
 
 public class _03_Array {
     public static void main(String[] args) {
-        int sum=0;
+        int total=0;
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i+1;
-            sum +=  arr[i];
+            total += arr[i];
         }
-        System.out.println(sum);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("arr 값 : "+Arrays.toString(arr));
+
+        System.out.println("arr 합 : "+total);
+        System.out.println("arr 평균 : "+(float)total/arr.length);
     }
 }
