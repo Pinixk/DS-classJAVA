f709dd5033cc08318ff4a394d2be1e2eefa1770b
Pinixk <bjkim0110@gmail.com>
2022-06-22 11:32:14 +0900

    06/22

--- workspace/Basic/src/Exercise/JExercise_04_09.java
+++ workspace/Basic/src/Exercise/JExercise_04_09.java
@@ -12,6 +12,7 @@ public class JExercise_04_09 {
         // }
         // System.out.println("sum=" + sum);
 
+        
         Scanner s = new Scanner(System.in);
         System.out.println("Input number");
         String input = s.nextLine();
@@ -19,18 +20,21 @@ public class JExercise_04_09 {
         int num = 0;
         int sum = 0;
         try{
-            num = Integer.parseInt(input)
+            num = Integer.parseInt(input);
         } catch(Exception e){
             System.out.println("숫자를 입력하지 않아 종료합니다.");
             System.exit(-1);
         }
 
-        int tmp = 0;
-        while(tmp>=10){
-            tmp = num/10;
-            sum += tmp%10;
+        while(true){
+            if(num/10 == 0){    // 10보다 작은 수를 10으로 나누면 몫은 0(int이기 때문) 
+                sum += num;
+                break;
+            } else {
+                sum += num%10;  // 10으로 나누고 나머지를 합함
+                num = num/10;   // 10으로 나눈 몫을 tmp에 담고 loop
+            }
         }
-
         System.out.println("sum=" + sum);
 
     }
