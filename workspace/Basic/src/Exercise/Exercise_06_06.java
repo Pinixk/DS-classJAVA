e26912938a4f82f8bf199047c33eec8022309e14
Pinixk <bjkim0110@gmail.com>
2022-06-23 17:44:15 +0900

    06/23

---
+++ workspace/Basic/src/Exercise/Exercise_06_06.java
@@ -0,0 +1,23 @@
+package Exercise;
+class MyPoint{
+    int x;
+    int y;
+    
+    MyPoint(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getDistance(int x1, int y1) {
+        return Math.sqrt(Math.pow(x1-x,2)+Math.pow(y1-y,2));
+        // return Exercise_06_04.getDistance(x, y, i, j);
+    }
+}
+
+
+public class Exercise_06_06 {
+    public static void main(String[] args) {
+        MyPoint p = new MyPoint(1, 1);
+        System.out.println(p.getDistance(2,2));
+    }
+}
