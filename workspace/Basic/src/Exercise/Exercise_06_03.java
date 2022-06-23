e03fb08ac5fc841ce76bebaae106d0710650a22c
Pinixk <bjkim0110@gmail.com>
2022-06-23 17:19:19 +0900

    06/23

---
+++ workspace/Basic/src/Exercise/Exercise_06_03.java
@@ -0,0 +1,34 @@
+package Exercise;
+
+public class Exercise_06_03 {
+    public static void main(String[] args) {
+        Student1 s = new Student1();
+        s.name = "홍길동";
+        s.ban = 1;
+        s.no = 1;
+        s.kor = 100;
+        s.eng = 60;
+        s.math = 76;
+
+        System.out.println("이름 : " + s.getName());
+        System.out.println("총점 : " + s.getTotal());
+        System.out.println("평균 : " + s.getAverage());
+    }
+}
+
+class Student1{
+    String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+
+    public Student1(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name; this.ban = ban;this.no=no;this.kor=kor;this.eng=eng;
+        this.math = math;
+      }
+      
+    public String getAverage() {return (int)(getTotal()/3.0f*10.0f+0.5f)/10.0f;}
+    public String getTotal() {return kor+eng+math;}
+}
