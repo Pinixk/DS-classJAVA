b6430d08752db6fd5a9d33e9bcae4135ea6234a4
Pinixk <bjkim0110@gmail.com>
2022-06-23 12:18:41 +0900

    06/23

--- workspace/Basic/src/_03_Object/_03_Getter_Setter.java
+++ workspace/Basic/src/_03_Object/_03_Getter_Setter.java
@@ -1,5 +1,7 @@
 package _03_Object;
 
 public class _03_Getter_Setter {
-    
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ workspace/Basic/src/_03_Object/_04_Static_Instance.java
@@ -0,0 +1,5 @@
+package _03_Object;
+
+public class _04_Static_Instance {
+    
+}
