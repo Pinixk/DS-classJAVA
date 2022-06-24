09823a6cb86f23b88e07dbeeb0c07563da404158
Pinixk <bjkim0110@gmail.com>
2022-06-24 09:16:48 +0900

    06/24

--- workspace/Basic/src/Exercise/Exercise_06_17.java
+++ workspace/Basic/src/Exercise/Exercise_06_17.java
@@ -1,5 +1,22 @@
 package Exercise;
 
 public class Exercise_06_17 {
-    
+    static int[] shuffle(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int rand = (int) (Math.random() * arr.length);
+            int tmp = arr[i];
+            arr[i] = arr[rand];
+            arr[rand] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[] original = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        System.out.println(java.util.Arrays.toString(original));
+
+        int[] result = shuffle(original);
+        System.out.println(java.util.Arrays.toString(result));
+    }
 }
---
+++ workspace/Basic/src/Exercise/Exercise_06_18.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+public class Exercise_06_18 {
+    static boolean isNumber(String str){
+        if(str==null || str.equals("")) return false;
+        
+        for (int i = 0; i < str.length(); i++) {
+            char cha = str.charAt(i);
+
+            if(cha<'0' || cha >'9') return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "123";
+        System.out.println(str+"는 숫자입니까?"+isNumber(str));
+
+        str = "1234o";
+        System.out.println(str+"는 숫자입니까?"+isNumber(str));
+    }
+}
