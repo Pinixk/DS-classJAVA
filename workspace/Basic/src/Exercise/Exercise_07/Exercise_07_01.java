521446b159f907c3bc8602e809d4ffbdd0b06409
Pinixk <bjkim0110@gmail.com>
2022-06-24 17:40:39 +0900

    06/24

--- workspace/Basic/src/Exercise/Exercise_07/Exercise_07_01.java
+++ workspace/Basic/src/Exercise/Exercise_07/Exercise_07_01.java
@@ -1,36 +1,38 @@
 package Exercise.Exercise_07;
 
-public class Exercise_07_01 {
-    SutdaDeck deck = new SutdaDeck();
-    
-    for(int i =0; i<deck.cards.length; i++){
-        System.out.println(deck.cards[i]+",");
-    }
-}
-
-class SutdaDeck{
+class SutdaDeck {
     final int CARD_NUM = 20;
     SutdaCard[] cards = new SutdaCard[CARD_NUM];
 
-    SutdaDeck(){
-        
+    SutdaDeck() {
+        int
     }
 }
 
-class SutdaCard{
+class SutdaCard {
     int num;
     boolean isKwang;
 
-    SutdaCard(){
+    SutdaCard() {
         this(1, true);
     }
 
-    SutdaCard(int num, boolean isKwang){
+    SutdaCard(int num, boolean isKwang) {
         this.num = num;
         this.isKwang = isKwang;
     }
 
-    public String toString(){
+    public String toString() {
         return num + (isKwang ? "K" : "");
+    }
+}
+
+public class Exercise_07_01 {
+    SutdaDeck deck = new SutdaDeck();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < deck.cards.length; i++) {
+            System.out.println(deck.cards[i] + ",");
+        }
     }
 }
