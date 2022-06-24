1135b6f713327fdc2e96e864f32dfc0a58ba4de7
Pinixk <bjkim0110@gmail.com>
2022-06-24 16:11:13 +0900

    06/24

--- workspace/Basic/src/_03_Object/extention/_13_InnerClass.java
+++ workspace/Basic/src/_03_Object/extention/_13_InnerClass.java
@@ -1,19 +1,33 @@
 package _03_Object.extention;
 
 public class _13_InnerClass {
-    
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+
+        Outer.InstanceInner oi = outer.new InstanceInner();
+        System.out.println(oi.iv);  // 100
+
+        Outer.StaticInner os = new Outer.StaticInner();
+        System.out.println(os.iv);  // 200
+
+        outer.myMethod();   // 400
+    }
 }
 
 class Outer{
     class InstanceInner{
-
+        int iv = 100;
     }
+
     static class StaticInner{
-
+        int iv = 200;
+        static int sv = 300;
     }
+
     void myMethod(){
         class localInner{
-            
+            int iv = 400;
         }
+        System.out.println(new localInner().iv);
     }
 }
