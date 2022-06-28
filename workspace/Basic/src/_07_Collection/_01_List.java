d9d6a528d7e1d276b3bab0375f97cfae94e97fff
Pinixk <bjkim0110@gmail.com>
2022-06-28 14:01:33 +0900

    06/28

--- workspace/Basic/src/_07_Collection/_01_List.java
+++ workspace/Basic/src/_07_Collection/_01_List.java
@@ -1,21 +1,42 @@
 package _07_Collection;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class _01_List {
    public static void main(String[] args) {
-      ArrayList list = new ArrayList();
-      list.add(new Date());
-      list.add("Hello");
-      list.add(true);
-      list.add(1);
-      list.clear();
-      System.out.println(list);
+      
+      // generics 비설정
+      @SuppressWarnings("rawtypes")
+      ArrayList al = new ArrayList();
+      // al.add(new Date()); 
+      // al.add("Hello");
+      // al.add(true);
+      // al.add(1);
+      al.clear();
+      System.out.println(al);
 
+
+      // generics 설정
+      ArrayList<Integer> list = new ArrayList<>();
+      
       for (int i = 1; i <= 10; i++) {
          list.add(i);
       }
+
+      int total = 0;
+      for (int i : list) { 
+      // generics를 int로 설정했기에 int로 받을 수 있음
+      // generics 설정 안 했을 시 Object로 받을 수 있음
+         total += i;
+      }
+
+      total = 0;
+      for (int i= 0; i < list.size(); i++) {  
+         total += list.get(i);
+         // 형변환 없이 generics타입으로 연산 가능
+      }
+
       System.out.println(list);
+      System.out.println(total);
    }
 }
