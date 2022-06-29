c97353270730afbf0170a478e038b0b125d24579
Pinixk <bjkim0110@gmail.com>
2022-06-30 08:51:13 +0900

    06/30

--- workspace/Basic/src/_09_thread/_06_Thread_suspend_resume_stop.java
+++ workspace/Basic/src/_09_thread/_06_Thread_suspend_resume_stop.java
@@ -3,10 +3,12 @@ package _09_thread;
 public class _06_Thread_suspend_resume_stop {
 	public static void main(String[] args) {
 		RunImpl r = new RunImpl();
-		Thread t1 = new Thread(r,"*"); 
-		Thread t2 = new Thread(r,"**");
-		Thread t3 = new Thread(r,"***");
-		t1.start();t2.start();t3.start();
+		Thread t1 = new Thread(r, "*");
+		Thread t2 = new Thread(r, "**");
+		Thread t3 = new Thread(r, "***");
+		t1.start();
+		t2.start();
+		t3.start();
 		try {
 			Thread.sleep(2000);
 			t1.suspend();
@@ -19,18 +21,20 @@ public class _06_Thread_suspend_resume_stop {
 			t2.stop();
 			Thread.sleep(2000);
 			t3.stop();
-		} catch (InterruptedException e) {}
+		} catch (InterruptedException e) {
+		}
 	}
 }
 
 class RunImpl implements Runnable {
 	@Override
 	public void run() {
-		while(true) {
+		while (true) {
 			System.out.println(Thread.currentThread().getName());
 			try {
 				Thread.sleep(1000);
-			} catch (InterruptedException e) { }
+			} catch (InterruptedException e) {
+			}
 		}
 	}
 }
