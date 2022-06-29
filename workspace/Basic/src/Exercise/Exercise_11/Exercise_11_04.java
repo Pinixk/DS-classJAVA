24301fd5f5009e5ca15dfcbb4defca4e3c92f1e7
Pinixk <bjkim0110@gmail.com>
2022-06-29 11:36:33 +0900

    06/29

--- workspace/Basic/src/Exercise/Exercise_11/Exercise_11_04.java
+++ workspace/Basic/src/Exercise/Exercise_11/Exercise_11_04.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Exercise11_4 {
    public static void main(String[] args) {
-      ArrayList list = new ArrayList();
+      ArrayList<Object> list = new ArrayList<>();
       list.add(new Student("이자바", 2, 1, 70, 90, 70));
       list.add(new Student("안자바", 2, 2, 60, 100, 80));
       list.add(new Student("홍길동", 1, 3, 100, 100, 100));
