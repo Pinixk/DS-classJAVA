7e465dcbcbb89bf98e86ea74437c887b0b1a2c5e
PinixK <bjkim0110@gmail.com>
2022-06-29 20:22:16 +0900

    06/29

--- workspace/Basic/src/_09_thread/_08_Thread_synchronized.java
+++ workspace/Basic/src/_09_thread/_08_Thread_synchronized.java
@@ -10,10 +10,17 @@ public class _08_Thread_synchronized {
 
 class Account {
 	private int balance = 1000;
-	public int getBalance() {return balance;}
+
+	public int getBalance() {
+		return balance;
+	}
+
 	synchronized public void withdraw(int money) {
-		if(balance >= money) {
-			try {Thread.sleep(1000);} catch (InterruptedException e) {}
+		if (balance >= money) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
 			balance -= money;
 		}
 	}
@@ -21,17 +28,13 @@ class Account {
 
 class Customer implements Runnable {
 	Account acc = new Account();
+
 	@Override
 	public void run() {
-		while(acc.getBalance()>0) {
-			int money = (int)(Math.random()*3+1)*100;
+		while (acc.getBalance() > 0) {
+			int money = (int) (Math.random() * 3 + 1) * 100;
 			acc.withdraw(money);
-			System.out.println("잔액:"+acc.getBalance()+acc);
+			System.out.println("잔액:" + acc.getBalance() + acc);
 		}
 	}
 }
-
-
-
-
-
