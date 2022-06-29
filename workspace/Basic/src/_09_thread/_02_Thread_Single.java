29440b9ef8f91e7f55bdfcbb92b6ab486b7709fd
Pinixk <bjkim0110@gmail.com>
2022-06-29 15:10:12 +0900

    06/29

---
+++ workspace/Basic/src/_09_thread/_02_Thread_Single.java
@@ -0,0 +1,20 @@
+package _09_thread;
+
+public class _02_Thread_Single {
+   public static void main(String[] args) {
+      long startTime = System.currentTimeMillis(); // 시작
+
+      System.out.println(startTime);
+
+      for (int i = 0; i < 300; i++) {
+         System.out.printf("%s", new String("-"));
+      }
+
+      System.out.println(System.currentTimeMillis() - startTime); // 중간
+
+      for (int i = 0; i < 300; i++) {
+         System.out.printf("%s", new String("|"));
+      }
+      System.out.println(System.currentTimeMillis() - startTime); // 끝
+   }
+}
--- workspace/Basic/src/_09_thread/_02_Thread_SingleMulti.java
+++
@@ -1,7 +0,0 @@
-package _09_thread;
-
-public class _02_Thread_SingleMulti {
-   public static void main(String[] args) {
-      System.out.println(System.currentTimeMillis());      
-   }
-}
