e83882beb47770ee76182977cf84bc096e4358b9
Pinixk <bjkim0110@gmail.com>
2022-06-30 12:11:24 +0900

    06/30

--- workspace/Basic/src/_10_Lambda/_02_Functional_Interface.java
+++ workspace/Basic/src/_10_Lambda/_02_Functional_Interface.java
@@ -11,40 +11,41 @@ public class _02_Functional_Interface {
    public static void main(String[] args) {
       Supplier<Integer> s = () -> (int) (Math.random() * 100) + 1; // 매개변수 X, 반환값 O
       Consumer<Integer> c = i -> System.out.print(i + ","); // 매개변수 O, 반환값 X
-      Function<Integer, Integer> f = i -> i / 10 * 10;
-      Predicate<Integer> p = i -> i % 2 == 0;
+      Predicate<Integer> p = i -> i % 2 == 0;               // 조건식, 매개변수 O, 반환값 boolean
+      Function<Integer, Integer> f = i -> i / 10 * 10;      // 함수, 매개변수 O, 반환값 O
+
 
       List<Integer> list = new ArrayList<>();
-
+      // Supplier
       makeRandomList(s, list);
       System.out.println(list);
-
-      PrintEventNum(p, c, list);
-      
+      // Predicate, Consumer
+      PrintEventNum(c, p, list);
+      // Function
       List<Integer> newList = doSomething(f, list);
       System.out.println(newList);
 
    }
 
+   private static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+      for (int i = 0; i < 10; i++) {
+         list.add(s.get());
+      }
+   }
+   
+   private static <T> void PrintEventNum(Consumer<T> c, Predicate<T> p, List<T> list) {
+      System.out.print("[");
+      for (T i : list) {
+         if (p.test(i))
+         c.accept(i);
+      }
+      System.out.println("]");
+   }
+   
    private static <T> List<T> doSomething(Function<T, T> f, List<T> list) {
       List<T> newList = new ArrayList<T>(list.size());
       for (T i : list)
          newList.add(f.apply(i));
       return newList;
-   }
-
-   private static <T> void PrintEventNum(Predicate<T> p, Consumer<T> c, List<T> list) {
-      System.out.print("[");
-      for (T i : list) {
-         if (p.test(i))
-            c.accept(i);
-      }
-      System.out.println("]");
-   }
-
-   static <T> void makeRandomList(Supplier<T> s, List<T> list) {
-      for (int i = 0; i < 10; i++) {
-         list.add(s.get());
-      }
    }
 }
