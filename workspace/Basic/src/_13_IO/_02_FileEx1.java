4a0be6c202dc39690876baa67e66d0cbeff76787
Pinixk <bjkim0110@gmail.com>
2022-07-01 17:36:47 +0900

    07/01

---
+++ workspace/Basic/src/_13_IO/_02_FileEx1.java
@@ -0,0 +1,25 @@
+package _13_IO;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+
+public class _02_FileEx1 {
+	public static void main(String[] args) throws IOException {
+		JFileChooser fc = new JFileChooser();
+		int ret = fc.showOpenDialog(null);
+		if (ret == 0) {
+			File f = fc.getSelectedFile();
+			System.out.println("FileName: "+f.getName());
+			System.out.println("Extend: "+
+						f.getName().substring(f.getName().lastIndexOf(".")+1));
+			System.out.println("path: "+f.getPath());
+			System.out.println("Absolute path: "+f.getAbsolutePath());
+			System.out.println("Canonical path: "+f.getCanonicalPath());
+			System.out.println("Directory Name: "+f.getParent());
+			System.out.println("pathSeparator: "+File.pathSeparator);
+			System.out.println("separatorChar: "+File.separatorChar);
+		}
+	}
+}
