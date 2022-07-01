0ebf494bb1f1bc26522855ade2878b55184007e1
Pinixk <bjkim0110@gmail.com>
2022-07-01 11:20:09 +0900

    07/01

--- workspace/Basic/src/_12_Swing/_02_Component.java
+++ workspace/Basic/src/_12_Swing/_02_Component.java
@@ -1,15 +1,34 @@
 package _12_Swing;
 
-public class _02_Component extends BaseFrm{
+import javax.swing.*;
+
+public class _02_Component extends BaseFrm {
   public static void main(String[] args) {
     new _02_Component();
   }
+
+  JPanel pnlNorth, pnlCenter;
+  JLabel lbMenu1, lbMenu2;
+  JLabel icon1, icon2;
+  JCheckBox cbMenu1, cbMenu2;
+
   @Override
   public void init() {
-    
+    pnlNorth = new JPanel();
+    lbMenu1 = new JLabel("Choice 1");
+    lbMenu2 = new JLabel("Choice 2");
+    cbMenu1 = new JCheckBox();
+    cbMenu2 = new JCheckBox();
+
+    pnlCenter = new JPanel();
+    icon1 = new JLabel();
+    icon2 = new JLabel();
   }
+
   @Override
   public void arrange() {
-    
+    pnlNorth.add(lbMenu1); pnlNorth.add(cbMenu1);
+    pnlNorth.add(lbMenu2); pnlNorth.add(cbMenu2);
+    add(pnlNorth,"North");
   }
 }
