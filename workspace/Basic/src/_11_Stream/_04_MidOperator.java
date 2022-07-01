09265e1cbfd1e6cb223f2ec13e78e28b321e0c7a
Pinixk <bjkim0110@gmail.com>
2022-07-01 09:19:12 +0900

    07/01

--- workspace/Basic/src/_11_Stream/_04_MidOperator.java
+++ workspace/Basic/src/_11_Stream/_04_MidOperator.java
@@ -6,17 +6,16 @@ import java.util.stream.Stream;
 public class _04_MidOperator {
   public static void main(String[] args) {
     Stream<String[]> strArrStream = Stream.of(
-      new String[]{"abc","def","ghi"},
-      new String[]{"ABC","DEF","GHI"}
-    );
+        new String[] { "abc", "def", "ghi" },
+        new String[] { "ABC", "DEF", "GHI" });
     // Stream<Stream<String>> strStream = strArrStream.map(Arrays::stream);
 
     Stream<String> strStream = strArrStream.flatMap(Arrays::stream);
-    strStream.map(String::toUpperCase).distinct().sorted().
-    forEach(System.out::println);
+    strStream.map(String::toUpperCase).distinct().sorted().forEach(System.out::println);
     System.out.println();
 
   }
 }
 
-class Value{}
+class Value {
+}
--- workspace/Basic/src/_11_Stream/_05_FinalOperator.java
+++ workspace/Basic/src/_11_Stream/_05_FinalOperator.java
@@ -16,20 +16,20 @@ public class _05_FinalOperator {
         new Student("소자바", 1, 200),
         new Student("닥자바", 3, 290),
         new Student("감자바", 3, 180));
-        Optional<Student> s = studentStream.reduce(new BinaryOperator<Student>() {
-          @Override
-          public Student apply(Student t, Student u) {
-            return t.totalScore > u.totalScore ? t : u;
-          }
-        });
-        if (s.isPresent())
-        System.out.println(s);
-
-        Student[] stdArr = studentStream.toArray(Student[]::new);
-        System.out.println(studentStream.count());
-        System.out.println(studentStream.collect(Collectors.counting()));
-        System.out.println(studentStream.collect(Collectors.summingInt(Student::getTotalScore)));
-        System.out.println(studentStream.collect(Collectors.maxBy(Comparator.comparingInt(Student::getTotalScore))));
-        System.out.println(studentStream.collect(Collectors.summarizingInt(Student::getTotalScore)));
+    Optional<Student> s = studentStream.reduce(new BinaryOperator<Student>() {
+      @Override
+      public Student apply(Student t, Student u) {
+        return t.totalScore > u.totalScore ? t : u;
       }
+    });
+    if (s.isPresent())
+      System.out.println(s);
+
+    Student[] stdArr = studentStream.toArray(Student[]::new);
+    System.out.println(studentStream.count());
+    System.out.println(studentStream.collect(Collectors.counting()));
+    System.out.println(studentStream.collect(Collectors.summingInt(Student::getTotalScore)));
+    System.out.println(studentStream.collect(Collectors.maxBy(Comparator.comparingInt(Student::getTotalScore))));
+    System.out.println(studentStream.collect(Collectors.summarizingInt(Student::getTotalScore)));
+  }
 }
