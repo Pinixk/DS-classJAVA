c3c46654a103ef5c81fac61c84a617adec894a27
Pinixk <bjkim0110@gmail.com>
2022-07-04 10:12:14 +0900

    07/04

--- workspace/Basic/src/_13_IO/Notepad.java
+++ workspace/Basic/src/_13_IO/Notepad.java
@@ -1,12 +1,16 @@
 package _13_IO;
 
 import javax.swing.*;
+import java.awt.event.*;
+
+
 import _12_Swing.BaseFrm;
 
 public class Notepad extends BaseFrm{
   public static void main(String[] args) {
     new Notepad();
   }
+  // 초기화
   private JMenuBar menuBar;
   private JMenu mnFile, mnInfo;
   private JMenuItem miOpen, miNew, miSave, miExit;
@@ -18,26 +22,30 @@ public class Notepad extends BaseFrm{
     menuBar = new JMenuBar();
     mnFile = new JMenu("File(F)");
     mnInfo = new JMenu("Info(I)");
-    miOpen = new JMenuItem("Open(O)");
+
     miNew = new JMenuItem("New(N)");
+    miOpen = new JMenuItem("Open(O)");
     miSave = new JMenuItem("Save(S)");
     miExit = new JMenuItem("Exit(E)");
+    
+    menuBar.add(mnFile);
+    menuBar.add(mnInfo);
+    mnFile.setMnemonic(KeyEvent.VK_F);
+    mnInfo.setMnemonic(KeyEvent.VK_I);
 
     mnFile.add(miNew);
     mnFile.add(miOpen);
     mnFile.add(miSave);
     mnFile.add(miExit);
 
-    menuBar.add(mnFile);
-    menuBar.add(mnInfo);
-
     setJMenuBar(menuBar);
 
     ta = new JTextArea();
-    scp = new JScrollPane();
+    scp = new JScrollPane(ta);
   }
   @Override
   public void arrange() {
     add(scp);
+    setTitle("My Memo");
   }
 }
