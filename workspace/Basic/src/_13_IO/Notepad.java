7371e0ae0f9f31ec8d19c42890fc4d113a68f7bc
Pinixk <bjkim0110@gmail.com>
2022-07-04 10:17:46 +0900

    07/04

--- workspace/Basic/src/_13_IO/Notepad.java
+++ workspace/Basic/src/_13_IO/Notepad.java
@@ -3,13 +3,13 @@ package _13_IO;
 import javax.swing.*;
 import java.awt.event.*;
 
-
 import _12_Swing.BaseFrm;
 
 public class Notepad extends BaseFrm{
   public static void main(String[] args) {
     new Notepad();
   }
+
   // 초기화
   private JMenuBar menuBar;
   private JMenu mnFile, mnInfo;
@@ -37,15 +37,21 @@ public class Notepad extends BaseFrm{
     mnFile.add(miOpen);
     mnFile.add(miSave);
     mnFile.add(miExit);
+    miNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+    miOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+    miSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+    miExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
 
     setJMenuBar(menuBar);
-
+    
     ta = new JTextArea();
     scp = new JScrollPane(ta);
   }
+
   @Override
   public void arrange() {
     add(scp);
     setTitle("My Memo");
   }
+
 }
