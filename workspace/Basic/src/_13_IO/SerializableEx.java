1b125ee9ca949d9210bc2cfe367ebbb93b4808eb
Pinixk <bjkim0110@gmail.com>
2022-07-04 12:13:46 +0900

    07/04

---
+++ workspace/Basic/src/_13_IO/SerializableEx.java
@@ -0,0 +1,66 @@
+package _13_IO;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class SerializableEx {
+  public static void main(String[] args) {
+    User u1 = new User("KBJ", 1);
+    User u2 = new User("AAA", 0);
+
+    ArrayList<User> list = new ArrayList<>();
+    list.add(u1);
+    list.add(u2);
+
+    String exportFile = "UserInfo.ser";
+    // Serializable Object Encoding
+    try { 
+      FileOutputStream fos = new FileOutputStream(exportFile);
+      BufferedOutputStream bos = new BufferedOutputStream(fos);
+      ObjectOutputStream out = new ObjectOutputStream(bos);
+
+      out.writeObject(u1);
+      out.writeObject(u2);
+      out.writeObject(list);
+      out.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // Serializable Object Decoding
+    try {
+      FileInputStream fis = new FileInputStream(exportFile);
+      BufferedInputStream bis = new BufferedInputStream(fis);
+      ObjectInputStream in = new ObjectInputStream(bis);
+
+      
+      User get1 = (User) in.readObject();
+      User get2 = (User) in.readObject();
+      ArrayList result = (ArrayList) in.readObject();
+
+      System.out.println(get1);
+      System.out.println(get2);
+      System.out.println(result);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+}
+
+class User implements Serializable{
+
+  String name;
+  int gender;
+
+  public User(){ this("Unknown",0);}
+  public User(String name, int gender){
+    this.name = name;
+    this.gender = gender;
+  }
+
+  public String toString(){return name+"/"+gender;}
+
+}
--- workspace/Basic/src/_13_IO/_02_FileEx1.java
+++ workspace/Basic/src/_13_IO/_02_FileEx1.java
@@ -11,15 +11,18 @@ public class _02_FileEx1 {
 		int ret = fc.showOpenDialog(null);
 		if (ret == 0) {
 			File f = fc.getSelectedFile();
-			System.out.println("FileName: "+f.getName());
-			System.out.println("Extend of File: "+
-						f.getName().substring(f.getName().lastIndexOf(".")+1));
-			System.out.println("path: "+f.getPath());
-			System.out.println("Absolute path: "+f.getAbsolutePath());
-			System.out.println("Canonical path: "+f.getCanonicalPath());
-			System.out.println("Directory Name: "+f.getParent());
-			System.out.println("pathSeparator: "+File.pathSeparator);
-			System.out.println("separatorChar: "+File.separatorChar);
+			// 3개를 자주 씀
+			System.out.println("FileName: " + f.getName());
+			System.out.println("path: " + f.getPath());
+			System.out.println("Directory Name: " + f.getParent());
+
+			System.out.println();
+			System.out.println("Extend of File: " +
+					f.getName().substring(f.getName().lastIndexOf(".") + 1));
+			System.out.println("Absolute path: " + f.getAbsolutePath());
+			System.out.println("Canonical path: " + f.getCanonicalPath());
+			System.out.println("pathSeparator: " + File.pathSeparator);
+			System.out.println("separatorChar: " + File.separatorChar);
 		}
 	}
 }
