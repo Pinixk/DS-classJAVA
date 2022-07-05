498c1b74621734ea1031a3cbd0e0e8c9d877b1e2
Pinixk <bjkim0110@gmail.com>
2022-07-05 09:26:21 +0900

    07/05

--- workspace/Basic/src/Test.java
+++ workspace/Basic/src/Test.java
@@ -9,7 +9,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-
 public class Test extends Base {
   public static void main(String[] args) {
     new Test();
@@ -22,7 +21,7 @@ public class Test extends Base {
   private DataOutputStream out;
   private String nickName;
 
-  public Test(){
+  public Test() {
     super("Chat room", 400, 500);
     String serverIp = JOptionPane.showInputDialog("Input Server Ip", "192.168.0.32");
     nickName = JOptionPane.showInputDialog("Input Your Nickname");
@@ -53,8 +52,8 @@ public class Test extends Base {
     tf.addActionListener(e -> {
       try {
         String tmp = tf.getText();
-        if(tmp.length()!=0 && tmp.trim().length()!=0)
-        out.writeUTF("["+nickName+"]"+tf.getText());
+        if (tmp.length() != 0 && tmp.trim().length() != 0)
+          out.writeUTF("[" + nickName + "]" + tf.getText());
         tf.setText("");
       } catch (Exception e1) {
         e1.printStackTrace();
@@ -73,7 +72,7 @@ public class Test extends Base {
   private class Receiver extends Thread {
     Socket socket;
     DataInputStream in;
-  
+
     public Receiver(Socket socket) {
       try {
         this.socket = socket;
@@ -82,7 +81,7 @@ public class Test extends Base {
         e.printStackTrace();
       }
     }
-  
+
     @Override
     public void run() {
       while (in != null) {
@@ -127,7 +126,7 @@ abstract class Base extends JFrame { // abstract 인스턴스 상속 불가
   public abstract void arrange();
 
   public void inflate() {
-    setSize(width, height);   // 창 크기 설정
+    setSize(width, height); // 창 크기 설정
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x 누르면 프로그램도 종료
     setLocationRelativeTo(this); // 창 정가운데 정렬
     setVisible(true); // 가시성
